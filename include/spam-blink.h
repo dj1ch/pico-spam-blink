88d9ea6b92a17fd3ffea6c4680dfe7f127fbf19d
dj1ch <tg.1ch0712@gmail.com>
2024-06-02 00:39:09 -0700

    add files

---
+++ include/spam-blink.h
@@ -0,0 +1,16 @@
+/**
+ * spam-blink.h
+ * header for spam-blink.c
+*/
+
+#ifndef SPAM_BLINK_H
+#define SPAM_BLINK_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include "pico/stdlib.h"
+#include "pico/stdio.h"
+
+int main();
+
+#endif // SPAM_BLINK_H
---
+++ src/spam-blink.c
@@ -0,0 +1,18 @@
+/**
+ * spam-blink.c
+ * make the pico blink
+*/
+
+#include "blink.h"
+
+int main() {
+    const uint8_t LED = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
+    while (true) {
+        gpio_put(LED, 1);
+        sleep_ms(1);
+        gpio_put(LED, 0);
+        sleep_ms(1);
+    }
+}
